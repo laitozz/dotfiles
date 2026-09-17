39c334cd18bade8a3ba86a1eec03e78fa764d485
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-09-17 10:44:46 +0300

    Update nh script to ask for password before activation

--- utils/nh.sh
+++ utils/nh.sh
@@ -3,6 +3,6 @@
 # From https://github.com/fish-shell/fish-shell/discussions/11667
 
 fish --init-command="function nhr --on-event fish_prompt
-    commandline -i 'nh os switch'
+    commandline -i 'sudo echo \"\" && nh os switch'
 end
 "
